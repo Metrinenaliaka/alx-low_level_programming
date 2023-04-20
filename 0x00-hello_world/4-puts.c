ed54e3014daa2b6263986f1980e80996b225971b
Metrinenaliaka <metrinenam@gmail.com>
2023-04-20 16:04:41 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program using puts to print out a line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
