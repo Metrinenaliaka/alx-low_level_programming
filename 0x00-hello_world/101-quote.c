3d6fb46a08f65cf40d1a9442251af82e9778c4db
Metrinenaliaka <metrinenam@gmail.com>
2023-04-20 17:49:04 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	char h = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	putchar(h);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
