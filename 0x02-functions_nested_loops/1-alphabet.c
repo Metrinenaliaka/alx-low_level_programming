64982e0689763b681c56cb1419778e93b0a0c492
Metrinenaliaka <metrinenam@gmail.com>
2023-04-25 10:16:40 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char az;
+
+	for (az = 97; az <= 122; az++)
+	{
+		_putchar(az);
+	{
+
+	_putchar('\n');
+	return (0);
+}
