96397008764d3c2464a8f4f5382364ff365f5a40
Metrinenaliaka <metrinenam@gmail.com>
2023-05-01 23:15:34 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98- function resets parameter to 98 using a pointer.
+ * @n: oarameter used to reset
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	_putchar("%d\n", *n);
+}
