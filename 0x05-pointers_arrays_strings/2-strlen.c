1e4ddf62bdbd2ff27d025d687efd51943fe49e06
Metrinenaliaka <metrinenam@gmail.com>
2023-05-02 02:52:06 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen- prints string length.
+ * @s: string whose length is to be determined
+ * Return: Integer
+ *
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
