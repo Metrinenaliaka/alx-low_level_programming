28ce7517e5a9c2a1f2af4163b7d14c8a928aeae6
Metrinenaliaka <metrinenam@gmail.com>
2023-05-02 14:30:49 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,15 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int a = 0;
 
-	while (*str != '\0')
+	while (str[a] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
 	}
 	_putchar('\n');
 }
