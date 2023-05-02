0725f8a1a630118b43e178c97c9a4ed82f6312a7
Metrinenaliaka <metrinenam@gmail.com>
2023-05-01 23:03:32 -0700

    putchar

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <main.h>
+/**
+* _putchar- writes the charachter c to stdout
+* @c: to be printed
+*
+* Return: 1 if successful
+*/
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
