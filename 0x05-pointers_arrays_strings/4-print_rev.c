8d5a7fd194b570d52e6a883e8edcc2368d2c0930
Metrinenaliaka <metrinenam@gmail.com>
2023-05-02 12:36:35 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,9 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (i >= 0)
+	for (s[i] != '\0'; i >= 0; i--)
 	{
-		if (s[i] == '\0')
-		{
-			break;
-		i++;
-		}
-	}
-	for (i--; i >= 0; i--)
 		_putchar(s[i]);
-
+	}
 	_putchar('\n');
 }
