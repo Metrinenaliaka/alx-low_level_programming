c1536b24ea3d911a43285440a81d6bb14e181ae0
Metrinenaliaka <metrinenam@gmail.com>
2023-05-02 16:18:58 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *puts_half- prints half of the string
+ *@str: string to be used
+ */
+void puts_half(char *str)
+{
+	int s = 0, z, l = 0;
+
+	while (str[s] != '\0')
+	{
+		l++;
+		i++;
+	}
+	z = l / 2;
+	if ((l % 2) == 1)
+	{
+		z = ((l + 1) / 2);
+	}
+	for (s = z; str[s] != '\0'; s++)
+	{
+		_putchar(str[s]);
+	}
+	_putchar('\n');
+}
