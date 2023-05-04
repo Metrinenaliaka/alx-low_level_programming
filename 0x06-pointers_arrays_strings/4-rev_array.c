e7b554d8e27af45e89628911d264958f08c52620
Metrinenaliaka <metrinenam@gmail.com>
2023-05-04 10:15:54 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array- reverses array contents
+ * @a: array to be reversed
+ * @n: number of elements in an array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int value;
+
+	for (i = 0; i <= n--; i++)
+	{
+		value = a[i];
+		a[i] = a[n];
+		a[n] = value;
+	}
+}
