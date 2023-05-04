b471c8526476023813a376c2bce070d8a3b8bc9f
Metrinenaliaka <metrinenam@gmail.com>
2023-05-04 06:50:27 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,23 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * *_strncat - concatenates two strings with n bytes.
- * @dest: string one
- * @src: string two
- * @n: n bytes to be used
- * Return: dest
- */
+* *_strncat - concatenates two strings with n bytes.
+* @dest: string one
+* @src: string two
+* @n: n bytes to be used
+* Return: dest
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, lgth;
+
+	lgth = strlen(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[lgth + i] = src[i];
+	}
+	dest[lgth + i] = '\0';
+	return (dest);
+}
 
