421c79d8ff16e81e4d04c65b86f8842c47e68bff
Metrinenaliaka <metrinenam@gmail.com>
2023-05-08 02:09:14 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - function finds the first occurrence of the substring
+ * @haystack: string to be checked
+ * @needle: substring to be checked
+ * Return: needle or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int ind1 = 0, ind2 = 0;
+
+	while ((haystack[ind2] != '\0') || (needle[ind1] != '\0'))
+	{
+		if (needle[ind1] != haystack[ind2])
+		{
+			ind2++;
+			ind1 = 0;
+		}
+		else
+		{
+			ind2++;
+			ind1++;
+		}
+	}
+	if (needle[ind1] == '\0')
+	{
+		return (needle);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
