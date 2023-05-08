93b02a8184c30e3f1602da09572dfbe2d419a19d
Metrinenaliaka <metrinenam@gmail.com>
2023-05-08 06:40:08 -0700

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *set_string- sets string to another value
+ *@s: string to change
+ *@to: string we are changing to
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
