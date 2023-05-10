ce3fddc3cc16e9b7e9ff44856f0f3a002ec6acc1
Metrinenaliaka <metrinenam@gmail.com>
2023-05-10 04:26:39 -0700

    Task 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,22 @@
 #include "main.h"
 #include <string.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string to be printed.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
+
 /**
  * is_palindrome - checks for a palidrome
  * @s: string to be checked
@@ -7,7 +24,7 @@
  */
 int is_palindrome(char *s)
 {
-	char rev = strsep(*s);
+	char rev = _print_rev_recursion(s);
 
 	if (*s == " ")
 	{
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * wildcmp- compares string
+ * @s1: string one
+ * @s2: string two
+ * Return: 1 if identical else return 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == '*' || *s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
