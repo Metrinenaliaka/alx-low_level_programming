bdc5111a1a8895550c835729dda75ae5e53aaf5e
Metrinenaliaka <metrinenam@gmail.com>
2023-05-10 00:16:10 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - finds the power of a number
+ * @x: integer one
+ * @y: integer to raise
+ * Return: return x otherwise return -1
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+		return (1);
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
