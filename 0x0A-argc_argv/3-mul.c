6a3e464bee3de1b64ff721fd001cd429df4c8086
Metrinenaliaka <metrinenam@gmail.com>
2023-05-12 03:50:53 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints its name, followed by a new line
+ *@argc: the size of commads
+ *@argv: array of commands
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+        b = atoi(argv[2]);
+        product = a * b;
+
+	printf("%d\n", product);
+	return (0);
+}
+
+
+	
