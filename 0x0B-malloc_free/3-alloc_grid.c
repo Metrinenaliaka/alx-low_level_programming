8ce1f7154f8a01555312cbc35aad45d01d9b84ff
Metrinenaliaka <metrinenam@gmail.com>
2023-05-15 07:26:16 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,16 +23,16 @@ int **alloc_grid(int width, int height)
 	}
 	else
 	{
-		for(i = 0; i < height; i++)
+		for (i = 0; i < height; i++)
 		{
 			p[i] = (int *)malloc(width * sizeof(int));
-			if (p[i]== NULL)
+			if (p[i] == NULL)
 			{
-				for (j = 0; j < i; j++)
+				/*for (j = 0; j < i; j++)
 				{
 					free(p[j]);
 				}
-				free(p);
+				free(p);*/
 				return (NULL);
 			}
 			for (j = 0; j < width; j++)
