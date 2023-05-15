1f5419b00ba7b0853280a5444c4488532e068b6a
Metrinenaliaka <metrinenam@gmail.com>
2023-05-15 03:44:13 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		return ("");
+		s1 = s2;
 	}
 	if (s2 == NULL)
 	{
-		return ("");
+		s2 = s1;
 	}
 
 	size = (strlen(s1) + strlen(s2)) + 1;
