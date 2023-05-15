69c4323d34f93c55816495ce5aeb743374a739cb
Metrinenaliaka <metrinenam@gmail.com>
2023-05-15 08:11:56 -0700

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid- frees a 2 dimensional grid
+ * @height: rows
+ * @grid: 2d grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid == NULL || height <= 0)
+	{
+		return;
+        }
+	do
+	{
+		free(grid[i]);
+		i++;
+	}
+	while (i < height);
+	free(grid);
+}
