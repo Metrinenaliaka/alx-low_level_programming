85645f1aaacaac3643f0ae0430c91755d7c51e61
Metrinenaliaka <metrinenam@gmail.com>
2023-05-16 08:53:07 -0700

    Task6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,89 +10,31 @@
 
 char **strtow(char *str)
 {
-        int cnt = 0;
-        int wrd = 0;
-        int i, j, Len;
-        char **word;
+	int i = 0, len;
+	char p = ' ';
+	char ** s;
+	char word = {};
 
-        if (str == NULL || *str == '\0')
-        {
-                return NULL;
-        }
-
-        // Count the number of words in the string
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                if (str[i] == ' ')
-                {
-                        wrd = 0;
-                }
-                else if (wrd == 0)
-                {
-                        wrd = 1;
-                        cnt++;
-                }
-        }
-
-        wors = (char **)malloc((cnt + 1) * sizeof(char *));
-        if (word == NULL)
-        {
-                return NULL;
-        }
-
-        wrd = 0;
-        Len = 0;
-        j = 0;
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                if (str[i] == ' ')
-                {
-                        if (wrd)
-                        {
-                                word[j] = (char *)malloc((Len + 1) * sizeof(char));
-                                if (word[j] == NULL)
-                                {
-                                        // Free memory if allocation fails
-                                        for (int i = 0; i < j; i++)
-                                        {
-                                                free(word[i]);
-                                        }
-                                        free(word);
-                                        return NULL;
-                                }
-                                strncpy(words[y], &str[x - wordLen], wordLen);
-                                words[y][wordLen] = '\0';
-                                y++;
-                                word = 0;
-                                wordLen = 0;
-                        }
-                }
-                else
-                {
-                        word = 1;
-                        wordLen++;
-                }
-        }
-
-        if (word)
-        {
-                words[y] = (char *)malloc((wordLen + 1) * sizeof(char));
-                if (words[y] == NULL)
-                {
-                        // Free memory if allocation fails
-                        for (int i = 0; i <= y; i++)
-                        {
-                                free(words[i]);
-                        }
-                        free(words);
-                        return NULL;
-                }
-                strncpy(words[y], &str[x - wordLen], wordLen);
-                words[y][wordLen] = '\0';
-                y++;
-        }
-
-        words[y] = NULL;
-
-        return words;
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+	len = strlen(str) + 1;
+	s = (char **)malloc(len * sizeof(char*));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] == '\0'; i++)
+	{
+		if (str[i] != p)
+		{
+			word = str[i];
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return (s);
 }
