7f4960e3cf76e16617d2709c0b0893172c2e8e4e
Metrinenaliaka <metrinenam@gmail.com>
2023-05-18 07:40:28 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,11 +16,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		return (NULL);
+		s2 = "";
 	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
  * main -  multiplies two positive numbers.
+ * @num1: first number
+ * @num2: second number
+ * @mul: result
+ * Return: 
  */
+
+int main(int argc, char *argv[])
+{
+	int * num1, num2;
+	int mul, i;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = argv[1];
+	num2 = argv[2];
+	for (i = 0; num1[i] != '\0' && num2[i] != '\0'; i++)
+	{
+		if (!isdigit(num1[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		if (!isdigit(num2[i]))
+                {
+                        printf("Error\n");
+                        exit(98);
+                }
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
+
+}
