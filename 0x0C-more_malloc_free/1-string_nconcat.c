cf6bfd95db0e57d0f349c7fc1dbd33ab883701fb
Metrinenaliaka <metrinenam@gmail.com>
2023-05-18 05:32:58 -0700

    Task 4

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,12 +14,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, len1, len2;
 	char *p;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	if (s1 == NULL || s2 == NULL)
-	{
-		s1 = "", s2 = "";
-	}
 	if (n >= len2)
 	{
 		n = len2;
