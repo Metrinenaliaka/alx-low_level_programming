b06ccd63a05cd3ed9929d829fd06abbdd27f7d3f
Metrinenaliaka <metrinenam@gmail.com>
2023-05-18 07:47:19 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,10 +34,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 	j = 0;
-	do {
+	while (j < len1)
+	{
 		p[j] = s1[j];
 		j++;
-	} while (j < len1);
+	}
 	for (i = 0; i < n; i++)
 	{
 		p[j + i] = s2[i];
