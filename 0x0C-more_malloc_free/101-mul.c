b74787c3a28eb5f86daac1cee3b98a0a2a9eb10d
Metrinenaliaka <metrinenam@gmail.com>
2023-05-18 11:51:21 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,32 +2,37 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
+#include "main.h"
 /**
- * main -  multiplies two positive numbers.
- * @num1: first number
- * @num2: second number
- * @mul: result
- * Return:
+ * main - multiplies two positive numbers.
+ * @argc: number of args
+ * @argv: arguments
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	int num1, num2, mul, i, j;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	if (!isdigit(num1) || !isdigit(num2))
-	{
-		printf("Error\n");
-		exit(98);
-	}
 	mul = num1 * num2;
 	printf("%d\n", mul);
 	return (0);
-
 }
