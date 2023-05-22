479881728c9a8b5e87b1d0d4123639dfe8272a4c
Metrinenaliaka <metrinenam@gmail.com>
2023-05-22 02:36:55 -0700

    Task 3

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * new_dog - creates a new dog
+ * @name: first
+ * @age: second
+ * @owner: third
+ * Return:
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
