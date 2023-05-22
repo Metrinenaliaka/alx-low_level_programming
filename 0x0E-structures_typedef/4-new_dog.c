38c5af146f46800ec412706caed87175eaca5a3e
Metrinenaliaka <metrinenam@gmail.com>
2023-05-22 03:31:30 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "dog.h"
+#include <string.h>
 /**
- * new_dog - creates a new dog
- * @name: first
- * @age: second
- * @owner: third
- * Return:
- */
+* new_dog - creates a new dog
+* @name: first
+* @age: second
+* @owner: third
+* Return:
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0, len = 0, len2 = 0, j = 0;
+	dog_t *new_dog;
 
+	while (*name == '\0')
+	{
+		len++;
+		i++;
+	}
+	new_dog = (dog_t *)malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	(*new_dog).name = (char *)malloc(len + 1);
+	if ((*new_dog).name == NULL)
+	{
+		return (NULL);
+	}
+	strcpy((*new_dog).name, name);
+	while (*owner == '\0')
+	{
+		len2++;
+		j++;
+	}
+	(*new_dog).owner = (char *)malloc(len2 + 1);
+	if ((*new_dog).owner == NULL)
+	{
+		return (NULL);
+	}
+	strcpy((*new_dog).owner, owner);
+	(*new_dog).age = age;
+	return (new_dog);
 }
