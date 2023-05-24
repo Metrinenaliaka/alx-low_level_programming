bc62040ed472334cbd0f67aebaee2ee3b4e350e4
Metrinenaliaka <metrinenam@gmail.com>
2023-05-24 05:47:38 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,9 +16,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	{
 		return;
 	}
-	do {
-		action(array);
+	for (i = 0; i <= size; i++)
+	{
+		action(*array);
 		array++;
-		i++;
-	} while (i < size);
+	}
 }
