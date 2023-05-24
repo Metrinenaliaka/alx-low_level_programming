2eaf9d58c332d62ddd0d2b987bedf59db22642b0
Metrinenaliaka <metrinenam@gmail.com>
2023-05-24 04:06:05 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,12 +4,16 @@
  *  on each element of an array
  *  @array: function
  *  @size: size of array
- *  *action: pointer to array
+ * @action: pointer to array
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
 	do {
 		action(*array);
 		array++;
