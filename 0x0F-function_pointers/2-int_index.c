ff3d52e6997cdd40825a400e9e7e32d3cd26df99
Metrinenaliaka <metrinenam@gmail.com>
2023-05-24 01:29:10 -0700

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index -  searches for an integer.
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer
+ * Return: 0, 1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (0);
+}
