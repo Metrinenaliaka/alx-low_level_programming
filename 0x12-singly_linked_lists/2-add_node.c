462268e0eba32dad54cb0242c69ce840bd1be5b7
Metrinenaliaka <Metrinenaliaka>
2023-06-02 04:11:28 -0700

    adds node at thestart

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_node - function that adds a new node at the beginning of a list_t
+ * @head: head node
+ * @str: data for new node
+ * Return: adress of new ele
+ *
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *p = malloc(sizeof(list_t));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	p->str = strdup(str);
+	if (p->str == NULL)
+	{
+		return (NULL);
+	}
+	p->len = strlen(str);
+	p->next = *head;
+	*head = p;
+	return (p);
+}
