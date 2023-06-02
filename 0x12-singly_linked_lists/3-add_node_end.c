27f9cea2185c652dedcafa100451e92aa0d7d56f
Metrinenaliaka <Metrinenaliaka>
2023-06-02 06:07:34 -0700

    corrected errors on the code

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,10 +13,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (p == NULL)
 		return (NULL);
 	p->str = strdup(str);
-	if (!p->str)
+/*	if (!p->str)
 	{
 		return (NULL);
-	}
+	}*/
 	p->len = strlen(str);
 	p->next = NULL;
 	if (*head == NULL)
