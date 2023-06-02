91ac9c9a87041fcb4c1b73b5e9da17438cd41e38
Metrinenaliaka <Metrinenaliaka>
2023-06-02 04:48:08 -0700

    corrected requirement errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,10 +15,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	p->str = strdup(str);
-	if (p->str == NULL)
+	/*if (p->str == NULL)
 	{
 		exit(98);
-	}
+	}*/
 	p->len = strlen(str);
 	p->next = *head;
 	*head = p;
