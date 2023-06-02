04c49378233fd98f2d0086b261de1ab3a1b5696f
Metrinenaliaka <Metrinenaliaka>
2023-06-02 06:16:00 -0700

    code to free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list.
+ * @head: head node
+ */
+void free_list(list_t *head);
+{
+	list_t *p;
+
+	p = head;
+	while (p != NULL)
+	{
+		head = head->next;
+		free(p->str);
+		free(p);
+		p = head;
+	}
+}
