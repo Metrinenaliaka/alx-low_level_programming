cb11b3d0c9916efb0575cccf4eeb65221b4b9f42
Metrinenaliaka <Metrinenaliaka>
2023-06-05 10:15:42 -0700

    corrected errors

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,6 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = current->next;
 		free(current);
 		current = NULL;
+		return (1);
 	}
 	else
 	{
@@ -30,6 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		previous->next = current->next;
 		free(current);
 		current = NULL;
+		return (1);
 	}
 	return (1);
 
