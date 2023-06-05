c7282293385f5581a47d9db22bc7942f9bf0a364
Metrinenaliaka <Metrinenaliaka>
2023-06-05 09:58:56 -0700

    deleting nodes

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+*delete_nodeint_at_index - deletes a node
+*@head: head node
+*@index: index to be deleted
+*Return: 1 or -1
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current = *head, *previous = *head;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		current = NULL;
+	}
+	else
+	{
+		while (index != 0)
+		{
+			previous = current;
+			current = current->next;
+			index--;
+		}
+		previous->next = current->next;
+		free(current);
+		current = NULL;
+	}
+	return (1);
+
+}
