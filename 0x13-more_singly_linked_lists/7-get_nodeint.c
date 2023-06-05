db3236576fc1bb41b2e8bc76c39bee9f4a48fb0d
Metrinenaliaka <Metrinenaliaka>
2023-06-05 09:14:32 -0700

    corrected errors

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index -  returns the nth node of 
+ * get_nodeint_at_index -  returns the nth node of
  * a listint_t linked list.
  * @head: head node
  * @index: index of the node, starting at 0
@@ -8,7 +8,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	int unsigned i = 0;
+	unsigned int i = 0;
 	listint_t *p;
 
 	if (head == NULL)
