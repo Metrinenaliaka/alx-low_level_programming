b10f3e545440cb6c7dfac3d40f3276d2fd50e0b4
Metrinenaliaka <Metrinenaliaka>
2023-06-05 07:49:59 -0700

    prints list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_listint_safe -  prints a listint_t linked list.
+ * @head: head node
+ * Return: number nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *p;
+
+	if (head == NULL)
+	{
+		exit(98);
+	}
+	p = NULL;
+	p = head;
+	while (p != NULL)
+	{
+		printf("[%p] %d\n", (void *)p, p->n);
+		count++;
+		if (p <= p->next)
+		{
+			printf("-> [%p] %d\n", (void *)p, p->next->n);
+			exit(98);
+		}
+		p = p->next;
+	}
+	return (count);
+}
