c33a2cd51eaae4f71555255934c442a8628d4eed
Metrinenaliaka <Metrinenaliaka>
2023-06-05 01:52:44 -0700

    Prints elements of a node list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: head node
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t  count = 0;
+	const listint_t *p;
+
+	if (h == NULL)
+	{
+		exit(98);
+	}
+	p = NULL;
+	p = h;
+	do {
+		printf("%d\n", p->n);
+		count++;
+		p = p->next;
+		
+	} while (p != NULL);
+	return (count);
+}
