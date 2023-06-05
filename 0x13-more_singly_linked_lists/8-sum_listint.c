d01beb9cfbc3950d0dd1f14f095f54b2fe651381
Metrinenaliaka <Metrinenaliaka>
2023-06-05 09:25:06 -0700

    corrected errors

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_listint -  function that returns the sum of all the 
+ * sum_listint -  function that returns the sum of all the
  * data (n) of a listint_t linked list.
  * @head: head node
  * Return: sum
@@ -10,6 +10,10 @@ int sum_listint(listint_t *head)
 	int sum = 0;
 	listint_t *p;
 
+	if (head == NULL)
+	{
+		return (0);
+	}
 	p = head;
 	do {
 		sum = sum + p->n;
