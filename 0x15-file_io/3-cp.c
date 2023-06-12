7d7d128b9139dc09896ded767d37196807333a9d
Metrinenaliaka <Metrinenaliaka>
2023-06-12 06:54:02 -0700

    Copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,11 +8,14 @@
 int main(int argc, char *argv[])
 {
 	FILE *file_from, *file_to;
-	char *buffer;
-	size_t buffer_size, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+	size_t bytes_read, bytes_written;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	file_from = fopen(argv[1], "rb");
 	if (file_from == NULL)
 	{
@@ -23,28 +26,25 @@ int main(int argc, char *argv[])
 	if (file_to == NULL)
 	{
 		fclose(file_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
-	buffer_size = BUFFER_SIZE, buffer = malloc(buffer_size);
-	if (buffer == NULL)
-	{
-		fclose(file_from), fclose(file_to);
-		dprintf(STDERR_FILENO, "Error: Failed to allocate memory\n"), exit(99);
-	}
-	while ((bytes_read = fread(buffer, 1, buffer_size, file_from)) > 0)
+	while ((bytes_read = fread(buffer, 1, sizeof(buffer), file_from)) > 0)
 	{
 		bytes_written = fwrite(buffer, 1, bytes_read, file_to);
 		if (bytes_written != bytes_read)
 		{
 			fclose(file_from), fclose(file_to);
-			dprintf(2, "Error: Failed to write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Failed to write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
 	if (ferror(file_from) || ferror(file_to))
 	{
 		fclose(file_from), fclose(file_to);
-		dprintf(STDERR_FILENO, "Error: File I/O error occurred\n"), exit(99);
+		dprintf(STDERR_FILENO, "Error: File I/O error occurred\n");
+		exit(99);
 	}
-	fclose(file_from), fclose(file_to), free(buffer);
+	fclose(file_from), fclose(file_to);
 	return (0);
 }
