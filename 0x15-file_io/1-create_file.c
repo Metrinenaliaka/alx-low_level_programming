ffa11dcf9801a4ef1af29853e08e5a1db296f45e
Metrinenaliaka <Metrinenaliaka>
2023-06-12 04:02:26 -0700

    corrected errors in code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,8 @@ int create_file(const char *filename, char *text_content)
 			len++;
 		}
 	}
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0_RDWR);
-	wrte = (file, text_content, ind);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 384);
+	wrte = (file, text_content, len);
 	if (file == -1 || wrte == -1)
 	{
 		return (-1);
