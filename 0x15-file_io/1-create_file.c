d63256013be1f56191851cf69a6550c450159b89
Metrinenaliaka <Metrinenaliaka>
2023-06-12 03:02:47 -0700

    Creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = fopen(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 	{
 		return (-1);
@@ -23,10 +23,10 @@ int create_file(const char *filename, char *text_content)
 		wbytes = write(file, text_content, len);
 		if (wbytes == -1)
 		{
-			fclose(file);
+			close(file);
 			return (-1);
 		}
 	}
-	fclose(file);
+	close(file);
 	return (1);
 }
