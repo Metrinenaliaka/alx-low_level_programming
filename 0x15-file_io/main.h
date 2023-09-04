1079600b4d88652543d428e916691ed89b583763
Metrinenaliaka <metrinenam@gmail.com>
2023-09-04 01:40:18 -0700

    Finale project

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,13 @@
 #define MAIN_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <elf.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
