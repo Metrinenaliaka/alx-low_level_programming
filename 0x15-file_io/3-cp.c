48975237491e6b141dc5b42ab4166996699041ee
Metrinenaliaka <metrinenam@gmail.com>
2023-09-04 02:34:03 -0700

    corrected errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,92 @@
 #include "main.h"
+
 /**
- * main - main function
- * @argc: number of args
- * @argv: array of args
- * Return: 0
+ * create_buffer - function that allocates 1024 bytes for a buffer.
+ * @file: name of the file buffer is storing chars for.
+ * Return: pointer to the newly-allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buf);
+}
+
+/**
+ * close_file - function that closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int i;
+
+	i = close(fd);
+
+	if (i == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - function that copies the contents of a file to another file.
+ * @argc: number of arguments to  supplied
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success.
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-	FILE *file_from, *file_to;
-	char *buffer;
-	size_t buffer_size = 1024, bytes_read, bytes_written;
+	int frm, to, rd, wrt;
+	char *buf;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	file_from = fopen(argv[1], "rb");
-	if (file_from == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	file_to = fopen(argv[2], "wb");
-	if (file_to == NULL)
-	{
-		fclose(file_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
-	buffer = (char *)malloc(buffer_size * sizeof(char));
-	if (buffer == NULL)
-	{
-		fclose(file_from), fclose(file_to);
-		dprintf(STDERR_FILENO, "Error: Failed to allocate memory\n"), exit(99);
-	}
-	while ((bytes_read = fread(buffer, 1, sizeof(buffer), file_from)) > 0)
-	{
-		bytes_written = fwrite(buffer, 1, bytes_read, file_to);
-		if (bytes_written != bytes_read)
+
+	buf = create_buffer(argv[2]);
+	frm = open(argv[1], O_RDONLY);
+	rd = read(frm, buf, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (frm == -1 || rd == -1)
 		{
-			fclose(file_from), fclose(file_to), free(buffer);
-			dprintf(STDERR_FILENO, "Error: Failed to write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buf);
+			exit(98);
 		}
-	}
-	if (ferror(file_from) || ferror(file_to))
-	{
-		fclose(file_from), fclose(file_to), free(buffer);
-		dprintf(STDERR_FILENO, "Error: File I/O error occurred\n"), exit(99);
-	}
-	fclose(file_from), fclose(file_to), free(buffer);
+
+		wrt = write(to, buf, rd);
+		if (to == -1 || wrt == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buf);
+			exit(99);
+		}
+
+		rd = read(frm, buf, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (rd > 0);
+
+	free(buf);
+	close_file(frm);
+	close_file(to);
 	return (0);
 }
